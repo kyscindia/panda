469a6142bc4ea02cc0e87babed122c88f4f55058
kyscindia <132968485+kyscindia@users.noreply.github.com>
2024-10-15 14:39:22 +0000

    tes 1

--- panda.c
+++ panda.c
@@ -34,45 +34,7 @@ int printFileNames(int numberOfFiles, char **fileNames) {
     return 0; //Easy
 }
 
-//Function to read and print from file 2
-int readFile(char *fileName) {
-    FILE *file = fopen(fileName, "r");
-    char line[256];
-    if(file != NULL){
-        while (fgets(line, 256, file)) { //Reads and prints each line from file 2 (Outside Help how to use fgets - Colab Log)
-        line[strcspn(line, "\n")] = '\0'; //why is there a space between all the names  ---------------------------------------------- (Outside Help)
-        printf("Name: %s\n", line); 
-        }
-    }
-    else{
-        return 0;
-    }
-    fclose(file);
-    return 3; //A little hard
-}
 
-//Function to read and print only 6 >= character words
-int extractSample(char *fileName) {
-    FILE *file = fopen(fileName, "r");
-    char line[256];
-    int lineNumber = 1;  // Starts counting from 1
-    if(file != NULL){
-        // Read each line from the file
-        while (fgets(line, 256, file)) {
-            line[strcspn(line, "\n")] = '\0';
-            // Check if the line has 6 or more characters
-            if (strlen(line) >= 6) { //how to use strlen ----------------------------------------------------------------- (Outside Help)
-                printf("Sample %d: %s\n", lineNumber, line);
-            }
-        lineNumber++;
-        }
-    }
-    else{
-        return 0;
-    }
-    fclose(file);
-    return 4; //Hardest Function
-}
 
 int main(int argc, char **argv) {
     int numOfArguments = argc - 1; // Skips file name (pandas.c) (Outside Help - Colab Log)
